759c1ef1956392f03dc5f00d3fb269c9dfbd26a5
eGuerreroT <55798086+eGuerreroT@users.noreply.github.com>
2026-06-10 17:37:31 -0600

    Add Services DI

---
+++ Outlander.Blazor/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Outlander.Blazor.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddOutlander(
+        this IServiceCollection services)
+    {
+        // Servicios futuros
+
+        // services.AddScoped<IOutlanderDialogService, OutlanderDialogService>();
+        // services.AddScoped<IOutlanderToastService, OutlanderToastService>();
+
+        return services;
+    }
+}
--- Outlander.Demo/Program.cs
+++ Outlander.Demo/Program.cs
@@ -1,3 +1,4 @@
+using Outlander.Blazor.Extensions;
 using Outlander.Demo.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddOutlander();
 
 var app = builder.Build();
 
